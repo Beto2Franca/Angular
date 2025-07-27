79b0862ea470d991025286108523a015165deffe
beto franca <beto2franca@gmail.com>
2025-07-27 14:46:10 -0300

    parte logica01

--- aula01/pagina01/src/app/calculadora/calculadora.ts
+++ aula01/pagina01/src/app/calculadora/calculadora.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss'
 })
 export class Calculadora {
+  n1: number = 0;
+  n2: number = 0;
+  resultado: number = 0;
 
+   calcularResultado(){
+    this.resultado = this.n1 + this.n2;
+   }
 }
