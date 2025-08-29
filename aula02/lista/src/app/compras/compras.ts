802749318e87f3be8edf189e3d44b7e42fc33c82
beto franca <beto2franca@gmail.com>
2025-08-29 10:53:00 -0300

    logica formulario

--- aula02/lista/src/app/compras/compras.ts
+++ aula02/lista/src/app/compras/compras.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './compras.html',
   styleUrl: './compras.scss'
 })
 export class Compras {
+      item: string = '';
+
+      adicionarItem(){
+        console.log('Item Recebido', this.item);
+        this.item='';
+      }
 
 }
